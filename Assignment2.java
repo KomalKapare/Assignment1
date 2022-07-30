d63e43fd48cc6771237d66f38f81ab2ebb037f9a
KomalKapare <110023635+KomalKapare@users.noreply.github.com>
2022-07-30 14:38:15 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,22 @@
+Java Program to Find Quotient and Remainder
+package javaassignment;
+
+/**
+ *
+ * @komal kapare
+ */
+public class Assignment2 
+{
+     public static void main(String[] args)
+    {
+       int x,y;
+            x = 10;
+            y = 5;
+           
+            int q = x / y;
+            int r = x % y;
+            System.out.println("Quotient : " + q);
+            System.out.println("Remainder : " + r);
+    }
+    
+}
