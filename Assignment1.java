1fa086b0da89a08a14c330ea8aa4d7d0b294794d
KomalKapare <110023635+KomalKapare@users.noreply.github.com>
2022-07-30 14:37:53 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,23 @@
+Java Program to Add, subtract, multiply and divide Two Numbers
+package javaassignment;
+public class Assignment1 
+{ public static void main(String[] args)
+    {
+         int x,y;
+       x = 10;
+       y = 5;
+       
+       System.out.println("Addition : ");
+       System.out.println(x + y);
+       
+       System.out.println("Subtraction : ");
+       System.out.println(x - y);
+       
+       System.out.println("Multiplication :");
+       System.out.println(x * y);
+       
+       System.out.println("Division : ");
+       System.out.println(x / y);
+    } 
+    
+}
